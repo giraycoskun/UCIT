a2fb22439210b2e255a41bbc126efe59a56986f6
giraycoskun <giraycoskun@sabanciuniv.edu>
2021-05-22 16:26:20 +0000

    half-done helper

--- src/helper.py
+++ src/helper.py
@@ -2,12 +2,15 @@ import re
 
 class Conversion:
     PARANTHESIS = {'(', ')'}
+    parameterPattern = None
+    operatorPattern = None
 
     def __init__(self, OPERATORS, PRIORITY={}):
 
         self.OPERATORS = OPERATORS
         self.PRIORITY = PRIORITY
         self.STACK = []
+        
 
     def __getElements(self, formula):
 
@@ -19,8 +22,9 @@ class Conversion:
         
         pattern = "[^" + pattern + "]+"
         matcher = re.compile(pattern)
+        self.parameterPattern = pattern
         elements = matcher.findall(formula)
-        print(elements)
+        
         return elements
         
     def reverse(self, formula):
@@ -43,14 +47,16 @@ class Conversion:
         formula = self.reverse(formula)
         
         output = ""
-
+        NUMBER_PARA = 0
         for character in formula:
             #print(character)
             if character.isalnum():
                 output += character
             elif character == '(':
+                output += '('
                 self.STACK.append('(')
             elif character in self.OPERATORS:
+
                 while (len(self.STACK) > 0 and self.PRIORITY[self.STACK[-1]] >= self.PRIORITY[character]):
                     output += self.STACK.pop()
                 self.STACK.append(character)
@@ -60,20 +66,41 @@ class Conversion:
                 while self.STACK[-1] != '(':
                     output += self.STACK.pop()
                 self.STACK.pop()
-                #output += ')'
+                output += ')'
         while len(self.STACK) > 0:
             output += self.STACK.pop()
         
-        print(output)
         formula = self.reverse(output)
         print(formula)
-        self.format(formula, elements)
+        print(elements)
+        formula = self.format(formula, elements)
+        print(formula)
         return formula
     
     def format(self, formula, elements):
-        
 
-        pass
+        matcher = self.getMatcher()
+        
+        check = True
+        while(check):
+            matched = matcher.search(formula)
+
+        for operator in self.OPERATORS:
+            formula = formula.replace(operator, (operator+ ' '))
+        for element in elements:
+            formula = formula.replace(element, (element+ ' '))
+        return formula
+    
+    def getMatcher(self):
+        print(self.parameterPattern)
+        pattern = "["
+        for op in self.OPERATORS:
+            pattern += op
+        pattern += "]"
+        pattern += self.parameterPattern
+        matcher = re.compile(pattern)
+        return matcher
+
                 
             
 
@@ -92,7 +119,7 @@ if __name__ == '__main__':
 
     C = Conversion(OPERATORS, PRIORITY)
 
-    formula = "(1+2)*(3+4)"
+    formula = "a+b+c+d"
     C.infix_2_prefix(formula)
 
 #REF
