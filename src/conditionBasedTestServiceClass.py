b0e57d04e1b005c4e8ee6fad9e0ab73422f4a23a
giraycoskun <giraycoskun@sabanciuniv.edu>
2021-05-23 16:22:15 +0000

    created condition based

--- src/conditionBasedTestServiceClass.py
+++ src/conditionBasedTestServiceClass.py
@@ -1,15 +1,160 @@
-if __name__ == '__main__':
-    test_space = {
+from py_expression_eval import Parser
+from helper2 import removeInvalidParenthesis, Conversion
+from os import walk
+import subprocess
+import re
+
+class ConditionTestServiceClass:
+    OPERATORS = {'&', '|', '!', '<', '>', '+', '-', '*', '/'}
+    PRIORITY = {
+        '*':5,
+        '/':5,
+        '-':4,
+        '+':4,
+        '>':3,
+        '<':3,
+        '!':2,
+        '&':1,
+        '|':1,
+        '(':0,
+        ')':0
+    }
+    
+    converter = Conversion(OPERATORS, PRIORITY)
+
+    def __init__(self, request_name, test_space):
+        self.input_filename = "./InputFiles/" + request_name + "_ConditonBasedTestInput.inFile"
+        self.test_space = test_space
+        self.entities = []
+        self.options = []
+        self.matcher = re.compile("[\|\&!]")
+        #self.matcher = re.compile("[^\(\)\&\|-]+")
+    
+    def getTestSet(self):
+        print("FUNCTION: getTestSet")
+
+        self.createTestSet()
+        return self.__parseTestSet()
+    
+    def __generateUCMCDCTestOutput(self, lines):
+        #print("FUNCTION: __generateUCMCDCTestOutputestSet")
+        test = dict()
+        for line in lines[1:]:
+            parsed = line.split()
+            option = parsed[2]
+            value = parsed[3][:-1]
+            if value in {'true', 'false'}:
+                value = value[0].upper() + value[1:]
+            
+            test[option] = eval(value)
+                
+            
+
+        return test
+    
+    def parseTestSet(self):
+        print("FUNCTION: parseTestCases")
+        output_path = "./ucitObject/"
+        path = walk(output_path)
+        test_cases = dict()
+        test_set = set()
+        for _, _, files in path:
+            count = 1
+            for file in files:
+                #print(file)
+                with open(output_path+file, "r") as file:
+                    lines = file.readlines()
+
+                    index = 0
+                    while(lines[index] !=  "# TEST CASE CONSTRAINTS\n" ):
+                        index += 1
+                    key = "TEST CASE: "+ str(count)
+                    count += 1
+                    test_cases[key] = self.__generateUCMCDCTestOutput(lines[index+1:])
+                    
+        return test_cases
+    
+    def createTestSet(self):
+        #print("FUNCTION: createTestSet")
+        self.createEntites()
+        self.createInputFile()
+        self.__runSolver()
+        return self.entities
+
+    def createEntites(self):
+        for func_str in self.test_space['conditions']:
+            options = self.matcher.split(func_str)
+            options = list(filter(lambda option: option not in {'&', '|', '!', ''}, options))
+            
+            for option in options:
+                if option not in self.options:
+                    self.options.append(option)
+                pos_converted_function = self.converter.infix_2_prefix(func_str)
+                neg_converted_function = self.converter.infix_2_prefix(func_str.replace(option, ('!'+option)))
+                
+                pos_func = self.converter.formatSugarOps(pos_converted_function)
+                neg_func = self.converter.formatSugarOps(neg_converted_function)
+
+
+                pos_entity = "( && {pos} (! {neg}) )".format(pos=pos_func, neg=neg_func)
+                neg_entity = "( && {neg} (! {pos}) )".format(pos=pos_func, neg=neg_func)
+                #print("ENTITY: ", pos_entity)
+                self.entities.append(pos_entity)
+                self.entities.append(neg_entity)
+
+    def createInputFile(self):
+        with open(self.input_filename, 'w') as file:
+            self.__writeHeader(file)
+            for index, entity in enumerate(self.entities):
+                self.__writeEntity(file, entity, index)
+
+    def __writeHeader(self, file):
+        entity_num = len(self.entities)
+        file.write("# NUMBER_OF_ENTITIES:{entity_num}\n".format(
+            entity_num=entity_num))
+        file.write("# SYSTEM_CONSTRAINTS_BEGIN")
+
+        for option in self.test_space['options']:
+            file.write('\n(bool ' + option + ')')
+        
+        for key, value in self.test_space['numbers'].items():
+            file.write('\n(int {number} {lower} {upper} )'.format(number=key, lower=value[0], upper=value[1]))
+
+        file.write("\n# SYSTEM_CONSTRAINTS_END\n\n")
+
+    def __writeEntity(self, file, entity, entityID, description=""):
+        description = 'Covering ' + entity.split('\n')[0]
+        file.write("# ENTITY_BEGIN\n")
+        file.write("# ENTITY_ID:{entityID}\n".format(entityID=entityID))
+        file.write("# ENTITY_DESCRIPTION: {description}\n".format(description=description))
+        print("\nENTITY: ", entity)
+        file.write(entity)
+        file.write("\n")
+        file.write("# ENTITY_END\n\n")
+
+    def __runSolver(self):
+        # "python main.pyc -m 1 -s solverOrderBased -i ./sampleInputFiles/solverBased.inFile"
+        command = ["python", "main.pyc", "-m", "1", "-s",
+                   "solverOrderBased", "-i", self.input_filename]
+        process = subprocess.run(command, stdout=subprocess.PIPE)
+        return process.returncode
+
+test_space = {
         "conditions":[
-            "(a<b)",
-            "(x&y)"
+            "(a<b)|(c<d)&!x",
         ],
 
         "numbers": {
             "a":[1, 20],
             "b":[1, 100],
-  
+            "c":[1, 20],
+            "d":[1, 100]
         }
         ,
         "options": ['x', 'y']
     }
+
+if __name__ == '__main__':
+    formula = "(a < c+b) or (c<d) and x"
+    test = ConditionTestServiceClass("giray", test_space)
+    test.parseTestSet()
---
+++ src/helper2.py
@@ -0,0 +1,219 @@
+import re
+from py_expression_eval import Parser
+
+    
+    
+class Conversion:
+    PARANTHESIS = {'(', ')'}
+    REPLACE_MAP = {
+            '&': 'and',
+            '|': 'or',
+            '!': 'not'
+    }
+
+    def __init__(self, OPERATORS, PRIORITY={}):
+
+        self.OPERATORS = OPERATORS
+        self.PRIORITY = PRIORITY
+        self.STACK = []
+
+        
+
+    def __getElements(self, formula):
+
+        pattern = ""
+        ops = {'(', ')'}
+        ops = ops.union(self.OPERATORS)
+        for element in ops:
+            pattern = pattern + '\\' + element
+        
+        pattern = "[^" + pattern + "]+"
+        matcher = re.compile(pattern)
+        self.parameterPattern = pattern
+        elements = matcher.findall(formula)
+        
+        return elements
+        
+    def reverse(self, formula):
+        def reverse_paranthesis(c):
+            if c == '(' or c == ')':
+                if c == '(':
+                    return ')'
+                else:
+                    return '('
+            else:
+                return c
+
+        formula = reversed(formula)
+        formula = list(map(reverse_paranthesis, formula))
+        return "".join(formula)
+
+    def __replaceRegular2PythonOperators(self, formula):
+
+        for key, value in self.REPLACE_MAP.items():
+            formula = formula.replace(key, (' '+value+' '))
+        return formula
+    
+    def __replacePython2RegularOperators(self, formula):
+
+        for key, value in self.REPLACE_MAP.items():
+            #print(key, value)
+            formula = formula.replace(value, key)
+            #print(formula)
+        return formula
+                
+    def infix_2_prefix(self, formula):
+
+        print("INPUT:", formula)
+
+        elements = self.__getElements(formula)
+        print("ELEMENTS:", elements)
+
+        formula = self.__replaceRegular2PythonOperators(formula)
+        print("1", formula)
+        parser = Parser()
+        formula = parser.parse(formula).toString()
+        print("2", formula)
+        formula = self.__replacePython2RegularOperators(formula)
+        print("3", formula)
+
+        formula = self.reverse(formula)
+        print("4", formula)
+        
+        output = ""
+        for character in formula:
+            #print(character)
+            if character.isalnum():
+                output += character
+            elif character == '(':
+                output += '('
+                self.STACK.append('(')
+            elif character in self.OPERATORS:
+
+                while (len(self.STACK) > 0 and self.PRIORITY[self.STACK[-1]] >= self.PRIORITY[character]):
+                    output += self.STACK.pop()
+                self.STACK.append(character)
+            elif character is ')':
+            # if character is ')'
+            #   then pop until '('.
+                while self.STACK[-1] != '(':
+                    output += self.STACK.pop()
+                self.STACK.pop()
+                output += ')'
+        while len(self.STACK) > 0:
+            output += self.STACK.pop()
+        
+        formula = self.reverse(output)
+        print("5:", formula)
+        formula = self.format(formula, elements)
+        print("OUTPUT: ", formula)
+        return formula
+    
+    def format(self, formula, elements):
+
+        formula = formula.replace('!(', '(!')
+        for operator in self.OPERATORS:
+            formula = formula.replace(operator, (operator+ ' '))
+        for element in elements:
+            formula = formula.replace(element, (element+ ' '))
+        return formula
+    
+    def getMatcher(self, elements):
+        element_pattern = ""
+        for item in elements:
+            element_pattern = element_pattern + item + '|'
+        element_pattern = '(' + element_pattern[:-1] + ')'
+        print(element_pattern)
+        op_pattern = ""
+        for op in self.OPERATORS:
+            op_pattern = op_pattern + '\\' + op
+        op_pattern = '[' + op_pattern + ']'
+        pattern = op_pattern + element_pattern + element_pattern
+        print(pattern)
+        matcher = re.compile(pattern)
+        #print(matcher.search("+a+b+cd"))
+        return matcher
+    
+    def formatSugarOps(self, formula):
+        formula = formula.replace('&', '&&')
+        formula = formula.replace('|', '||')
+        #formula = formula.replace('-', '!')
+        return formula
+    
+
+def isValidString(str):
+    cnt = 0
+    for i in range(len(str)):
+        if (str[i] == '('):
+            cnt += 1
+        elif (str[i] == ')'):
+            cnt -= 1
+        if (cnt < 0):
+            return False
+    return (cnt == 0)
+
+def isParenthesis(c):
+    return ((c == '(') or (c == ')'))
+      
+# method to remove invalid parenthesis 
+def removeInvalidParenthesis(str):
+    if (len(str) == 0):
+        return
+          
+    # visit set to ignore already visited 
+    visit = set()
+      
+    # queue to maintain BFS
+    q = []
+    temp = 0
+    level = 0
+      
+    # pushing given as starting node into queu
+    q.append(str)
+    visit.add(str)
+    while(len(q)):
+        str = q[0]
+        q.pop()
+        if (isValidString(str)):
+            #print(str)
+              
+            # If answer is found, make level true 
+            # so that valid of only that level 
+            # are processed. 
+            level = True
+        if (level):
+            continue
+        for i in range(len(str)):
+            if (not isParenthesis(str[i])):
+                continue
+                  
+            # Removing parenthesis from str and 
+            # pushing into queue,if not visited already 
+            temp = str[0:i] + str[i + 1:] 
+            if temp not in visit:
+                q.append(temp)
+                visit.add(temp)
+    return str
+
+#REFRENCES
+#https://www.geeksforgeeks.org/remove-invalid-parentheses/
+
+
+if __name__ == '__main__':
+
+    OPERATORS = {'&', '|', '-'}
+    PRIORITY = {
+        '-':2,
+        '&':1,
+        '|':1,
+        '(':0,
+        ')':0
+    }
+
+    formula = "a|(b&c)"
+    formula2 = "a|(b&-c)"
+    formula3 = "-a|(b&c)"
+    formula4 = "a|-(b&c)"
+
+    C = Conversion(OPERATORS, PRIORITY)
+    C.infix_2_prefix(formula2)
